58461204fa0d3e3dc11912d719fb609b2e5befa0
Karen <kaskar2008@mail.ru>
2018-10-01 02:57:05 +0300

    routes

---
+++ routes.js
@@ -0,0 +1,11 @@
+const UserController = require('./controllers/user');
+
+module.exports = {
+  'user': {
+    get: UserController.index,
+    post: UserController.handlePost
+  },
+  'user/custom/handler/test': {
+    get: UserController.myHandler
+  }
+};
