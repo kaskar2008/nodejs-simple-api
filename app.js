7ff2a2deb847f4999be1821d330353f4820216bf
Karen <kaskar2008@mail.ru>
2018-10-01 05:41:46 +0300

    server starting and configuration

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const APIServer = require('./server');
 const routes = require('./routes');
+const config = require('./config');
 
 class App {
   constructor() {
     this.server = new APIServer({
-      port: 5000,
-      mode: 'http',
+      port: config.port,
+      host: config.host,
+      mode: config.serverMode,
       routes
     });
   }
 
   async init() {
     try {
-      let { port, host } = await this.server.start();
-      console.log(`Server is up on ${host}:${port}`);
+      let info = await this.server.start();
+      let httpPostString = info.httpPort ? `\nhttp port: ${info.httpPort} ` : '';
+      let httpsPostString = info.httpsPort ? `\nhttps port: ${info.httpsPort} ` : '';
+      console.log(`Server is up (serverMode: ${config.serverMode}) on ${info.host} (${config.env} mode) ${httpPostString}${httpsPostString}`);
     } catch (e) {
       console.log(e);
     }
