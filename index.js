373e1849b6fdc3387562613ddc184a7354166bc3
Karen <kaskar2008@mail.ru>
2018-10-01 07:47:14 +0300

    entry point comments

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const app = require('./app');
 const bodyParser = require('./middlewares/body-parser');
 const testMiddleware = require('./middlewares/test');
 
+// Add some middlewares
 app.use(bodyParser);
 app.use(testMiddleware);
 
+// Initialize entire application
 app.init();
